078080a98877d501fa31612d334f0cef353d268a
Frank Dekervel <kervel@gmail.com>
2012-01-13 11:49:28 +0100

    name conventions

--- gmail-dl.py
+++ gmail-dl.py
@@ -22,6 +22,9 @@ def get_email(id):
         _, response = imap_server.fetch(id, '(UID BODY[TEXT])')
         return response[0][1]
                 
+def get_subject(id):
+        _, response = imap_server.fetch(id, '(body[header.fields (subject)])')
+        return  response[0][1][9:]
 
 def get_emails(email_ids):
     data = []
@@ -70,8 +73,13 @@ def findWho(body):
 
 def findType(body):
     x = re.compile("Artikel van het type ([A-Z]+) (Heen en Terug|Enkel), geldig in (.+) klas van (.+) naar (.+) op (.+) voor de prijs van (.+) EUR.")
-    return x.findall(body)
+    return x.findall(body)[0]
     
+def getFrom(type):
+	return type[3].replace("ZONE ","").replace("LEUVEN","LVN").replace("BRUSSEL","BXL")
+
+def getTo(type):
+	return type[4].replace("ZONE ","").replace("LEUVEN","LVN").replace("BRUSSEL","BXL")
 
 def findPrice(body):
     for l in body.splitlines():
@@ -80,6 +88,11 @@ def findPrice(body):
             k = k.replace(" EUR en wordt betaald door:","")
             k = k.replace(" en wordt betaald door:","")
             return k
+
+def findReferentie(subjectline):
+        x = re.compile("N ([0-9]+) ")
+        return x.findall(subjectline)[0]
+
 def findBetaaldDoor(body):
     bet = -1
     res = ""
@@ -97,18 +110,26 @@ def findBetaaldDoor(body):
 
 for id in emails_from("ticketonline"):
 	body = quopri.decodestring(get_email(id))
+	subject = quopri.decodestring(get_subject(id))
 	opa=  findOPA(body)
 	bestel= findOrder(body).replace(" ", "")
 	who = findWho(body)
 	betaald = findBetaaldDoor(body)
 	price = findPrice(body)
 	type = findType(body)
-	f = open("%s.txt" % bestel, "w")
+	ref = findReferentie(subject)
+	
+	suffix = ""
+	if (type[1] != "Enkel"):
+		suffix = "-T"
+	basename = "%s - NMBS - N%s - %s-%s%s %s" % (type[5].replace("/",""), ref, getFrom(type), getTo(type), suffix ,type[6].replace(",","") )
+	f = open("%s.txt" % basename, "w")
 	f.write(body)
 	f.close()
 	import os
-	os.system("enscript -o %s.ps  %s.txt" % (bestel,bestel))
-	os.system("ps2pdf %s.ps %s.pdf" % (bestel,bestel))
+	os.system("enscript -o '%s.ps'  '%s.txt'" % (basename,basename))
+	os.system("ps2pdf '%s.ps' '%s.pdf'" % (basename,basename))
+	os.system("rm '%s.txt' '%s.ps'" % (basename, basename))
 	print [opa, bestel, who, betaald,price,type]
 	
 	
